8593701f9e9371dea36fae12b4854fbc97c01533
Hrust <rg20031991@gmail.com>
2023-11-24 14:29:36 +0400

    improve layout style commit

---
+++ js/div-link/index.js
@@ -0,0 +1,29 @@
+    document.getElementById("myDiv").addEventListener("click", function() {
+
+    window.location.href = "http://127.0.0.1:5500/pages/orders/index.html";
+    
+    });
+
+    // after mouse click on "orders"  go to "orders "page
+
+
+    document.getElementById("wishlist").addEventListener("click", function() {
+
+     window.location.href = "http://127.0.0.1:5500/pages/wishlist/index.html";
+        
+    });
+
+    // after mouse click on "wishlist"  go to "favourite product" page
+
+   document.getElementById("history").addEventListener("click", function() {
+
+    window.location.href = "http://127.0.0.1:5500/pages/shopping_history/index.html";
+            
+    });
+    // after mouse click on "shopping history"  go to "shopping history"page
+
+    document.getElementById("log").addEventListener("click", function() {
+    window.location.href = "http://127.0.0.1:5500/pages/signIn/index.html";
+                
+    });
+    // after mouse click on "log out"  go to "log"page
